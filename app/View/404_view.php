80ebc9408820c47638821474446140dbc14f4b62
odium-bt <odium@riseup.net>
2026-09-02 15:08:38 +0200

    added 404 page

---
+++ app/View/404_view.php
@@ -0,0 +1,8 @@
+<?php require ROOT . '/app/View/header_view.php'; ?>
+<main class="content">
+    <article class="box">
+        <h2>Erreur 404</h2>
+        <p>La page que vous avez demandé n'existe pas...</p>
+    </article>
+</main>
+<?php require ROOT . '/app/View/footer_view.php'; ?>
