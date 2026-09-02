a0b54ad737884d9339ee8813b922477d555bff48
odium-bt <odium@riseup.net>
2026-09-02 15:08:47 +0200

    added dashboard draft

---
+++ app/View/accueil_view.php
@@ -0,0 +1,9 @@
+<?php require ROOT . '/app/View/header_view.php'; ?>
+<main class="content">
+    <article class="box">
+        <h2>Bonjour</h2>
+        <p><a href="?action=hardware">Liste du matériel</a></p>
+        <p><a href="?action=borrow">Faire une réservation</a></p>
+    </article>
+</main>
+<?php require ROOT . '/app/View/footer_view.php'; ?>
