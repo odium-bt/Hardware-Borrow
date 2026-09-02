1a1c980a0ff12ae109603b8a66610b2d99124f57
odium-bt <odium@riseup.net>
2026-09-02 15:06:23 +0200

    updated register fields

--- app/Controller/Register.php
+++ app/Controller/Register.php
@@ -11,45 +11,57 @@ use HardwareBorrow\Model\UserModel;
 class Register
 {
     public $errors = [];
-    protected string $username;
-    protected string $email;
-    protected string $emailConfirm;
-    protected string $password;
-    protected string $passwordConfirm;
+    protected string $first_name = "";
+    protected string $last_name = "";
+    protected string $email = "";
+    protected string $emailConfirm = "";
+    protected string $password = "";
+    protected string $passwordConfirm = "";
 
     public function __construct()
     {
-        // Contrôle de qualité (trouve les erreurs et ajoute les messages dans le tableau $errors)
         if (!empty($_POST)) {
             foreach ($_POST as $key => $post_element) {
                 switch ($key) {
-                    case 'username':
-                        $this->username = htmlspecialchars(trim($_POST['username'])) ?? null;
+                    case 'first_name':
+                        $this->first_name = trim($_POST['first_name']);
+                        break;
+                    case 'last_name':
+                        $this->last_name = trim($_POST['last_name']);
                         break;
                     case 'email':
-                        $this->email = strtolower(htmlspecialchars(trim($_POST['email']))) ?? null;
+                        $this->email = strtolower(trim($_POST['email']));
                         break;
                     case 'email-confirm':
-                        $this->emailConfirm = strtolower(htmlspecialchars(trim($_POST['email-confirm']))) ?? null;
+                        $this->emailConfirm = strtolower(trim($_POST['email-confirm']));
                         break;
                     case 'password':
-                        $this->password = htmlspecialchars(trim($_POST['password'])) ?? null;
+                        $this->password = trim($_POST['password']);
                         break;
                     case 'password-confirm':
-                        $this->passwordConfirm = htmlspecialchars(trim($_POST['password-confirm'])) ?? null;
+                        $this->passwordConfirm = trim($_POST['password-confirm']);
                 }
             }
 
-            // Nom d'utilisateur
-            if (!$this->username) {
-                $this->errors['username'] = "Requis";
-            } else  if (strlen($this->username) < 3) {
-                $this->errors['username'] = "Au moins 3 caractères";
-            } else if (strlen($this->username) > 100) {
-                $this->errors['username'] = "Moins de 100 caractères";
+            // Contrôle de qualité (trouve les erreurs et ajoute les messages dans le tableau $errors)
+            // Prénom
+            if ($this->first_name === "") {
+                $this->errors['first_name'] = "Requis";
+            } else  if (strlen($this->first_name) < 3) {
+                $this->errors['first_name'] = "Au moins 3 caractères";
+            } else if (strlen($this->first_name) > 100) {
+                $this->errors['first_name'] = "Moins de 100 caractères";
+            }
+            // Nom
+            if ($this->last_name === "") {
+                $this->errors['last_name'] = "Requis";
+            } else  if (strlen($this->last_name) < 3) {
+                $this->errors['last_name'] = "Au moins 3 caractères";
+            } else if (strlen($this->last_name) > 100) {
+                $this->errors['last_name'] = "Moins de 100 caractères";
             }
             // Email
-            if (!$this->email) {
+            if ($this->email === "") {
                 $this->errors['email'] = "Requis";
             } else if (strlen(($this->email)) > 254) {
                 $this->errors['email'] = "L'adresse entrée est trop longue";
@@ -57,13 +69,13 @@ class Register
                 $this->errors['email'] = "Veuillez entrer une adresse email valide";
             }
             // Confirmation d'email
-            if (!$this->emailConfirm) {
+            if ($this->emailConfirm === "") {
                 $this->errors['email-confirm'] = "Requis";
             } else if ($this->email != $this->emailConfirm) {
                 $this->errors['email-confirm'] = "Les email entrés ne sont pas identiques";
             }
             // Mot de passe
-            if (!$this->password) {
+            if ($this->password === "") {
                 $this->errors['password'] = "Requis";
             } else if (strlen($this->password) < 8) {
                 $this->errors['password'] = "Veuillez choisir un mot de passe de plus de 8 caractères";
@@ -71,7 +83,7 @@ class Register
                 $this->errors['password'] = "Veuillez choisir un mot de passe de moins de 128 caractères";
             }
             // Confirmation du mot de passe
-            if (!$this->passwordConfirm) {
+            if ($this->passwordConfirm === "") {
                 $this->errors['password-confirm'] = "Requis";
             } else if ($this->password != $this->passwordConfirm) {
                 $this->errors['password-confirm'] = "Les mots de passe entrés ne sont pas identiques";
@@ -91,7 +103,7 @@ class Register
 
             // Vérifie que l'adresse email ne soit pas déjà utilisée
             if ($user->isEmailUsed($this->email) === false) {
-                $user->registerUser($this->username, $this->email, $this->password); // envoie les infos utilisateur sur le modèle
+                $user->registerUser($this->first_name, $this->last_name, $this->email, $this->password); // envoie les infos utilisateur sur le modèle
                 require ROOT . '/app/View/inscription-success_view.php'; // affiche page succès
             } else {
                 // Affiche une erreur dans le formulaire d'inscription
